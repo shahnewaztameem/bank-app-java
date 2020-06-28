93f171ec857cee7829cf5b897df4a6a65b8957d9
shahnewaz tameem <shahnewaztamim@gmail.com>
2020-06-28 23:23:34 +0600

    Implement driver class

--- src/com/java/bankapp/Main.java
+++ src/com/java/bankapp/Main.java
@@ -3,6 +3,18 @@ package com.java.bankapp;
 public class Main {
 
     public static void main(String[] args) {
-        
+        Bank bank = new Bank("Bangladesh Bank");
+        bank.addBranch("Dhaka");
+        bank.addCustomer("Dhaka", "Shahnewaz", 5000);
+        bank.addCustomer("Dhaka", "Tameem", 4000);
+        bank.addCustomer("Dhaka", "Saied", 1000);
+
+        bank.addBranch("Khulna");
+        bank.addCustomer("Khulna", "Rafi", 2500);
+        bank.addCustomerTransaction("Dhaka", "Saied", 500);
+        bank.addCustomerTransaction("Khulna", "Tameem", 5500);
+        bank.addCustomerTransaction("Dhaka", "Shahnewaz", 5000);
+
+         bank.listCustomers("Dhaka", true);
     }
 }
